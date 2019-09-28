785dd28261b76fd76dd0ee4406b7d834805d3b9a
caltong <caltong1027@gmail.com>
2019-09-28 23:46:49 +0800

    train test split

--- vgg16_with_voc2012.py
+++ vgg16_with_voc2012.py
@@ -1,5 +1,16 @@
 from load_voc2012 import load_voc2012
-import os
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPool2D, Dense
+from keras.layers import LeakyReLU, Activation
+import keras
+from keras import backend as K
+import numpy as np
+from sklearn.model_selection import train_test_split
 
-load_voc2012()
+images, labels = load_voc2012()
+seed = 42
+np.random.seed(seed)
+print(images.shape, labels.shape)
 
+x_train, x_test, y_train, y_test = train_test_split(images, labels, test_size=0.1, random_state=seed)
+print(x_train.shape, x_test.shape, y_train.shape, y_test.shape)
