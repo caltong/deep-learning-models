78de39323da5b206c2cc1a72aa384fe9f3146f72
caltong <caltong1027@gmail.com>
2019-09-28 22:56:47 +0800

    onehot label

--- load_voc2012.py
+++ load_voc2012.py
@@ -5,6 +5,30 @@ import xml.etree.cElementTree as ET
 
 
 def load_voc2012():
+    """
+
+    :return: voc2012 train+val images, labels
+    """
+    dataset_classes = {'aeroplane': 0,
+                       'bicycle': 1,
+                       'bird': 2,
+                       'boat': 3,
+                       'bottle': 4,
+                       'bus': 5,
+                       'car': 6,
+                       'cat': 7,
+                       'chair': 8,
+                       'cow': 9,
+                       'diningtable': 10,
+                       'dog': 11,
+                       'horse': 12,
+                       'motorbike': 13,
+                       'person': 14,
+                       'pottedplant': 15,
+                       'sheep': 16,
+                       'sofa': 17,
+                       'train': 18,
+                       'tvmonitor': 19}
     dataset_path = os.path.join('data', 'voc2012')
     classification_path = os.path.join(dataset_path, 'VOCdevkit', 'VOC2012', 'ImageSets', 'Main', 'trainval.txt')
     image_path = os.path.join(dataset_path, 'VOCdevkit', 'VOC2012', 'JPEGImages')
@@ -24,7 +48,6 @@ def load_voc2012():
     images = np.array(images)
 
     labels = {}
-
     for label_name in label_names:
         tree = ET.parse(os.path.join(label_path, label_name))
         root = tree.getroot()
@@ -33,10 +56,16 @@ def load_voc2012():
             for name in child:
                 if name.tag == 'name':
                     label.append(name.text)
-
         labels[str(label_name).rstrip('.xml')] = list(set(label))
 
-    return images, labels
+    label_onehot = []
+    for key, value in labels.items():
+        onehot = [0] * 20
+        for classes in value:
+            onehot[dataset_classes[classes]] = 1
+        label_onehot.append(onehot)
+
+    return images, label_onehot
 
 
 images, labels = load_voc2012()
